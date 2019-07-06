8081fd7278137bc59fc11de14aeb01aabe896b51
Tanner Stokes <tanner@tannr.com>
2019-07-05 23:11:06 -0400

    Formatting

--- SafeEject/SafeEject/AppDelegate.swift
+++ SafeEject/SafeEject/AppDelegate.swift
@@ -11,10 +11,8 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-    }
+    func applicationDidFinishLaunching(_ aNotification: Notification) {}
+    
+    func applicationWillTerminate(_ aNotification: Notification) {}
 
 }
